73b334cb253a28ad4311c14e8e848485a76703c5
SuSanee <anshikaagarwal766@gmail.com>
2025-12-16 22:57:06 +0530

    feat: add user and video models, integrate cloudinary, and implement multer middleware

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,7 +8,6 @@ class ApiError extends Error {
     super(message);
     this.statusCode = statusCode;
     this.data = null;
-    this.message = message;
     this.success = false;
     this.errors = errors;
 
@@ -16,7 +15,7 @@ class ApiError extends Error {
         this.stack = stack
     }
     else{
-        Error.captureStackTrace(this, this.contructor)
+        Error.captureStackTrace(this, this.constructor)
     }
   }
 }
